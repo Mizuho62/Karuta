0a4d730397ff71863bf12056677504dd06fd0784
Mizuho62 <ohuzimoti62@gmail.com>
2024-01-14 19:40:05 +0900

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,18 @@
 Rails.application.routes.draw do
 
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- resources :games do
-  resources :likes, only: [:create, :destroy]
-  resources :comments, only: [:create]
-end
-resources :practices 
-
-
+  resources :games do
+   resources :likes, only: [:create, :destroy]
+   resources :comments, only: [:create]
+  end
+ 
+  resources :practices 
   resources :blogs
 
   devise_for :users, controllers: {   registrations: 'users/registrations',
   sessions: 'users/sessions' }
 
   resources :users, only: [:show] # ユーザーマイページへのルーティング
-
-  
   root 'game#index'
-  post 'games' => 'games#create'
 end
 
