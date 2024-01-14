d29fff4fea1455ce79df451cfce3a5db308a3322
Mizuho62 <ohuzimoti62@gmail.com>
2024-01-14 18:33:26 +0900

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,6 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id]) 
-      end
-    
-    def mypage
-      redirect_to action: "show", id:current_user.id
     end
-    
 
 end
